0534a5f7cc20153eebfbe8114b9c5f90cab37463
James Smith <james@lumenlearning.com>
2020-04-07 15:21:03 -0600

    add multiple dropdowns

--- src/com/lumenlearning/codereview/grader.kt
+++ src/com/lumenlearning/codereview/grader.kt
@@ -1,18 +1,29 @@
 package com.lumenlearning.codereview
 
-public fun grade(question: Question, choices: List<Choice>, response: Response): Double {
+public fun grade(question: Question, choices: List<List<Choice>>, response: Response): Double {
     if (question.questionType == QuestionType.MULTIPLE_CHOICE) {
-        val chosenResponse = choices.find { choice -> choice.key == response.keys.first() }
+        val chosenResponse = choices.first().find { choice -> choice.key == response.keys.first() }
         if (chosenResponse != null && chosenResponse.correct) {
-            return 1.0;
+            return 1.0
         } else {
-            return 0.0;
+            return 0.0
         }
     } else if (question.questionType == QuestionType.MULTIPLE_ANSWERS) {
-        val correctResponses = choices.filter { choice -> choice.correct }
-        val chosenResponses = choices.filter { choice -> response.keys.contains(choice.key) && choice.correct }
-        return chosenResponses.count().toDouble() / correctResponses.count().toDouble()
+        val correctChoices = choices.first().filter { choice -> choice.correct }
+        val chosenResponses = choices.first().filter { choice -> response.keys.contains(choice.key) && choice.correct }
+        return chosenResponses.count().toDouble() / correctChoices.count().toDouble()
+    } else if (question.questionType == QuestionType.MULITPLE_DROPDOWNS) {
+        val correctResponses = choices.mapIndexed { index, dropdownChoice ->
+            val currentResponse = response.keys[index]
+            val currentChosenChoice = dropdownChoice.find { choice -> currentResponse == choice.key }
+            if (currentChosenChoice != null && currentChosenChoice.correct) {
+                1.0
+            } else {
+                0.0
+            }
+        }.sum()
+        return correctResponses / choices.size.toDouble()
     } else {
-        return 0.0;
+        return 0.0
     }
 }
