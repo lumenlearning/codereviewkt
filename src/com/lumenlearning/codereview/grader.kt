c758edc3bcf828e9b241a8b4bc7f086de616a52c
James Smith <james@lumenlearning.com>
2020-04-07 14:45:06 -0600

    add mulitple answer question

--- src/com/lumenlearning/codereview/grader.kt
+++ src/com/lumenlearning/codereview/grader.kt
@@ -2,12 +2,16 @@ package com.lumenlearning.codereview
 
 public fun grade(question: Question, choices: List<Choice>, response: Response): Double {
     if (question.questionType == QuestionType.MULTIPLE_CHOICE) {
-        val chosenResponse = choices.find { choice -> choice.key == response.key  }
+        val chosenResponse = choices.find { choice -> choice.key == response.keys.first() }
         if (chosenResponse != null && chosenResponse.correct) {
             return 1.0;
         } else {
             return 0.0;
         }
+    } else if (question.questionType == QuestionType.MULTIPLE_ANSWERS) {
+        val correctResponses = choices.filter { choice -> choice.correct }
+        val chosenResponses = choices.filter { choice -> response.keys.contains(choice.key) && choice.correct }
+        return chosenResponses.count().toDouble() / correctResponses.count().toDouble()
     } else {
         return 0.0;
     }
--- test/com/lumenlearning/codereview/GraderTest.kt
+++ test/com/lumenlearning/codereview/GraderTest.kt
@@ -3,20 +3,45 @@ package com.lumenlearning.codereview
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class GraderTest {
     val multipleChoiceQuestion = Question(QuestionType.MULTIPLE_CHOICE, "What is the answer?")
-    val choices = listOf(Choice("A", "A", true), Choice("B", "B", false))
+    val multipleChoiceChoices = listOf(Choice("A", "A", true), Choice("B", "B", false))
 
     @Test
     @DisplayName("Multiple Choice should return 1.0 for a correct answer")
-    fun testMulitpleChoiceCorrectChoice() {
-        assertEquals(1.0, grade(multipleChoiceQuestion, choices, Response("A")))
+    fun testMultipleChoiceCorrectChoice() {
+        assertEquals(1.0, grade(multipleChoiceQuestion, multipleChoiceChoices, Response(listOf("A"))))
     }
 
     @Test
     @DisplayName("Multiple Choice should return 1.0 for an incorrect answer")
-    fun testMulitpleChoiceIncorrectChoice() {
-        assertEquals(0.0, grade(multipleChoiceQuestion, choices, Response("B")))
+    fun testMultipleChoiceIncorrectChoice() {
+        assertEquals(0.0, grade(multipleChoiceQuestion, multipleChoiceChoices, Response(listOf("B"))))
+    }
+
+    val multipleAnswerQuestion = Question(QuestionType.MULTIPLE_ANSWERS, "What is the answer?")
+    val multipleAnswerChoices = listOf(
+        Choice("A", "A", true),
+        Choice("B", "B", false),
+        Choice("C", "C", true))
+
+    @Test
+    @DisplayName("Multiple Answer should return 1.0 for a correct answer")
+    fun testMultipleAnswerCorrectChoice() {
+        assertEquals(1.0, grade(multipleAnswerQuestion, multipleAnswerChoices, Response(listOf("A", "C"))))
+    }
+
+    @Test
+    @DisplayName("Multiple Answer should return 1.0 for an incorrect answer")
+    fun testMultipleAnswerIncorrectChoice() {
+        assertEquals(0.0, grade(multipleAnswerQuestion, multipleAnswerChoices, Response(listOf("B"))))
+    }
+
+    @Test
+    @DisplayName("Mulitple Answer should return partial score for partially correct answer")
+    fun testMulitpleAnswerPartiallyCorrectChoice() {
+        assertEquals(0.5, grade(multipleAnswerQuestion, multipleAnswerChoices, Response(listOf("A"))))
     }
 }
